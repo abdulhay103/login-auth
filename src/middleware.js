8782ce1313479b92e6b32a91ba001c5db3110cab
abdulhay103 <abdulhay.me@gmail.com>
2023-09-04 11:14:47 +0600

    problem catching

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,14 @@
-import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
+import { NextResponse } from "next/server";
 
 export async function middleware(req, res) {
-  if (req.nextUrl.pathname.startsWith("/api/test/postmanTokenAuth")) {
+  if (req.nextUrl.pathname.startsWith("/api/dashboard")) {
     try {
       const reqHeaders = new Headers(req.headers);
       const token = reqHeaders.get("token");
       const key = new TextEncoder().encode(process.env.JWT_SECRET);
-      const decodedString = await jwtVerify(token, key);
+      // const decodedString = await jwtVerify(token, key);
+
       console.log(token);
       return NextResponse.next();
     } catch (error) {
